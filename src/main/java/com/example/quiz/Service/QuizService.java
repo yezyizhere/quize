66079779848abfa2ad9e22985ccb24e8c442a261
MIDZY <itmidnal0212@gmail.com>
2025-02-11 12:22:31 +0000

    시간 제한

--- src/main/java/com/example/quiz/Service/QuizService.java
+++ src/main/java/com/example/quiz/Service/QuizService.java
@@ -16,35 +16,35 @@ public class QuizService {
         // 문제 1
         list.add(new QuizQuestion(
                 "문제 1번의 예시",
-                Arrays.asList("1번 답안","2번 답안","3번 답안","4번 답안"),
+                Arrays.asList("1번 답안","2번 답안","3번 답안","4번 답안", "모르겠음"),
         0,
                 "https://search.pstatic.net/common?type=f&size=206x206&quality=95&direct=true&src=http%3A%2F%2Fshop1.phinf.naver.net%2F20210418_136%2F1618674583729CsTje_JPEG%2F54810200578891611_-1854514124.jpg"
                 ));
         // 문제 2
         list.add(new QuizQuestion(
                 "문제 2번의 예시",
-                Arrays.asList("1번 답안","2번 답안","3번 답안","4번 답안"),
+                Arrays.asList("1번 답안","2번 답안","3번 답안","4번 답안", "모르겠음"),
                 1,
                 "https://search.pstatic.net/common?type=f&size=206x206&quality=95&direct=true&src=http%3A%2F%2Fshop1.phinf.naver.net%2F20210420_119%2F1618917792831Sb33v_JPEG%2F55053401463382237_1240304716.jpeg"
                 ));
         // 문제 3
         list.add(new QuizQuestion(
                 "문제 3번의 예시",
-                Arrays.asList("1번 답안","2번 답안","3번 답안","4번 답안"),
+                Arrays.asList("1번 답안","2번 답안","3번 답안","4번 답안", "모르겠음"),
                 2,
                 "https://search.pstatic.net/common?type=f&size=206x206&quality=95&direct=true&src=http%3A%2F%2Fshop1.phinf.naver.net%2F20200724_215%2F1595581349044jAHJA_JPEG%2F31716943718608376_1431937637.jpeg"
                 ));
         // 문제 4
         list.add(new QuizQuestion(
                 "문제 4번의 예시",
-                Arrays.asList("1번 답안","2번 답안","3번 답안","4번 답안"),
+                Arrays.asList("1번 답안","2번 답안","3번 답안","4번 답안", "모르겠음"),
                 3,
                 "https://search.pstatic.net/common?type=f&size=206x206&quality=95&direct=true&src=http%3A%2F%2Fshop1.phinf.naver.net%2F20210522_279%2F1621673915289mxlar_JPEG%2F57809532425488875_-1425368071.jpg"
                 ));
         // 문제 5
         list.add(new QuizQuestion(
                 "문제 5번의 예시",
-                Arrays.asList("1번 답안","2번 답안","3번 답안","4번 답안"),
+                Arrays.asList("1번 답안","2번 답안","3번 답안","4번 답안", "모르겠음"),
                 3,
                 "https://search.pstatic.net/common?type=f&size=206x206&quality=95&direct=true&src=http%3A%2F%2Fshop1.phinf.naver.net%2F20200806_20%2F1596683152967IlYFi_JPEG%2F32818768910658433_478020071.jpeg"
                 ));
